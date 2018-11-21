2ffbb180552fb02931bf0b9d885722135643eed7
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-11-21 19:44:33 +0530

    Added Chirag's registration form code

--- emblaze/emblaze/views/views_common.py
+++ emblaze/emblaze/views/views_common.py
@@ -19,7 +19,7 @@ app.secret_key = 'secretkeyhereplease'
 # Rahul's
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index-chiggy.html")
 
 def allowedFile(filename):
     return '.' in filename and \
