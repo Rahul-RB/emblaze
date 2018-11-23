4645ee5096286902daf4a9d03c0c0db297343415
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-11-23 13:41:03 +0530

    Full integration complete

--- emblaze/emblaze/views/writeToYaml.py
+++ emblaze/emblaze/views/writeToYaml.py
@@ -1,8 +1,11 @@
+from emblaze.ResumeGenerator import resume
+rgPath = resume.__path__._path[0]
+
 import yaml
 def writeToYaml(inpDict):
     print(inpDict)
-    with open("test.yml", "w") as outfile:
+    with open(rgPath+"/data.yml", "w") as outfile:
         outfile.write("/* #*/ export const PERSON = `\n")
-    with open("test.yml", "a") as outfile:
+    with open(rgPath+"/data.yml", "a") as outfile:
         yaml.dump(inpDict, outfile, default_flow_style=False)
         outfile.write("`")
