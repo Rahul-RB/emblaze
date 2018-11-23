c1a7e0fd06886edec779bb8ce57915d16e4ac15b
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-11-23 18:30:39 +0530

    Added Submission Throttling-2

--- emblaze/emblaze/static/js/detailFiller.js
+++ emblaze/emblaze/static/js/detailFiller.js
@@ -105,4 +105,51 @@ $(document).ready(function() {
 			}	
 		});
 	});
+
+	$(".companySearch").each(function(index, el) {
+		var $this = $(this);
+		$(this).on('input', function(event) {
+			$(".companySearchResults").empty();
+			event.preventDefault();
+			/* Act on the event */
+			if($(this).val().length >=3){
+				var inpData = {
+					search : $(this).val(),
+					infoType : "companies"
+				}
+				$.ajax({
+					url: 'getInfo',
+					type: 'GET',
+					dataType: 'json',
+					data: inpData,
+				})
+				.done(function(data) {
+					console.log("<GET-1>success\n",data);
+					console.log("$this\n",$this);
+					var $thisSibling = $this.siblings(".companySearchResults");
+					console.log("$thisSibling\n",$thisSibling);
+					
+					$.each(data, function(index, val) {
+						console.log(val);
+						$thisSibling.append("<div class='searchRes' style='background-color:grey;cursor:pointer;margin-bottom:1px;'>"+val+"</div>");
+						$(".searchRes").each(function(index,el2){
+							$(this).on('click', function(event) {
+								event.preventDefault();
+								/* Act on the event */
+								console.log("Res:",$(this).text());
+								$this.val($(this).text());
+								$(this).parent(".companySearchResults").empty()
+							});
+						});	
+					});
+				})
+				.fail(function() {
+					console.log("error");
+				})
+				.always(function() {
+					console.log("complete");
+				});
+			}	
+		});
+	});
 });
