cb062b36970a28fb41b1ab69aebf4c6100893c2c
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-11-23 17:33:02 +0530

    Added predictive fetch onscroll

--- emblaze/emblaze/static/js/index.js
+++ emblaze/emblaze/static/js/index.js
@@ -10,11 +10,45 @@ $(document).ready(function() {
         if ($(window).scrollTop() >= topNavbar && !topNavbarReached) {
             console.log("made it 1");
             topNavbarReached = true;
+            $.ajax({
+                url: 'getAboutContents',
+                type: 'GET',
+            })
+            .done(function(data) {
+                console.log("<GET-1:>success",data);
+                $("#about").append(data["content"]);
+                // var imageUrl = "{{ url_for('static',filename='img/about4.jpg') }}";
+                var imageUrl = 'static/img/about4.jpg';
+                $("#back1").css("background-image", "url(" + imageUrl + ")");
+            })
+            .fail(function() {
+                console.log("error");
+            })
+            .always(function() {
+                console.log("complete");
+            });
         }
 
         if ($(window).scrollTop() >= mainText) {
             console.log("made it 2");
             mainTextReached = true;
+            $.ajax({
+                url: 'getClientContents',
+                type: 'GET',
+            })
+            .done(function(data) {
+                console.log("<GET-2:>success",data);
+                $("#clients").append(data["content"]);
+                // var imageUrl = "{{ url_for('static',filename='img/about4.jpg') }}";
+                var imageUrl = "static/img/background2.jpg";
+                $("#back2").css("background-image", "url(" + imageUrl + ")");
+            })
+            .fail(function() {
+                console.log("error");
+            })
+            .always(function() {
+                console.log("complete");
+            });
         }
         if(topNavbarReached && mainTextReached)
         {
